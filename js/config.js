b17e110b49f71de2b78d839a18cb3a318541cf1a
fliky-ai <Tordaulet16@gmail.com>
2026-06-22 01:50:28 +0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,22 @@
+hereconst CONFIG = {
+    SERVER_URL: "https://a38499-be46.m.jrnm.app",
+    CREATOR_ID: "8771009385",
+    CREATOR_USERNAME: "dauletbr",
+    MAX_RECONNECT_ATTEMPTS: 5,
+    VERSION: "1.0.0"
+};
+
+const tg = window.Telegram.WebApp;
+tg.expand();
+tg.ready();
+
+const tgUser = tg.initDataUnsafe?.user || {
+    id: 123456789,
+    first_name: "Локальный",
+    last_name: "Пользователь",
+    username: "тест_юзер",
+    photo_url: ""
+};
+
+const MY_ID = String(tgUser.id);
+let MY_USERNAME = tgUser.username || '';
